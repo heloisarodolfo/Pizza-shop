728bf20aad2e1401788d0f2e3681f6cff154aba9
heloisarodolfo <heloisacristinarodolfo@gmail.com>
2024-12-10 14:51:05 -0300

    Uploading front-end

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes";
+import "./index.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { Toaster } from "sonner";
+import { ThemeProvider } from "./components/theme/theme-provider";
+
+export function App() {
+  return (
+    <HelmetProvider>
+      <ThemeProvider storageKey="pizzashop-theme" defaultTheme="dark">
+        <Toaster richColors />
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+      </ThemeProvider>
+    </HelmetProvider>
+  );
+}
